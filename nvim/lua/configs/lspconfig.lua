cc57272b76af6c063d1080cb98ca2cce4b5fa2aa
kshcherban <konstantin.shcherban@unzer.com>
2025-04-07 17:33:39 +0200

    change pyright to ruff

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "gopls", "marksman", "pyright", "terraformls", "rust_analyzer" }
+local servers = { "html", "cssls", "gopls", "marksman", "ruff", "terraformls", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
