f98074e6a77db030fb7f779187afe40634403550
kshcherban <kostyantyn.shcherban@auto1.com>
2025-08-01 09:47:12 +0200

    Update mappings to repeat last macro with ,

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,3 +7,5 @@ map("n", "<C-d>", "<C-d>zz", { desc = "jump half screen down" })
 map("n", "<C-u>", "<C-u>zz", { desc = "jump half screen up" })
 -- map("n", "n", "nzz", { desc = "jump to next occurence" })
 -- map("n", "N", "Nzz", { desc = "jump to previous occurence" })
+-- repeat last command with dot
+map("n", ",", "@:")
