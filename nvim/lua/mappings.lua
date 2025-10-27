1366c9b6703d68db1e86974037c7d14444c03146
kshcherban <kostyantyn.shcherban@auto1.com>
2025-10-27 16:34:02 +0100

    Add terminal keymap

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -9,5 +9,15 @@ map("n", "<C-u>", "<C-u>zz", { desc = "jump half screen up" })
 -- map("n", "N", "Nzz", { desc = "jump to previous occurence" })
 -- Exit terminal mode with <Esc>
 map("t", "<Esc>", vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true))
+-- Go to the first terminal buffer
+map("n", "<leader>T", function()
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    if name:match "^term://" then
+      vim.cmd("buffer " .. bufnr)
+      return
+    end
+  end
+  vim.notify("No terminal buffer found", vim.log.levels.WARN)
 -- repeat last command with dot
 map("n", ",", "@:")
